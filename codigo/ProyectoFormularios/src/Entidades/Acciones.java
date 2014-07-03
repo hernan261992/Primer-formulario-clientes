c22e14347199ad780fea596b550e1f8945dbc9e8
Pedro Elias Charun Advincula <sail3@localhost.localdomain>
2014-07-02 22:40:54 -0500

    Agregada las funciones CRUD

--- codigo/ProyectoFormularios/src/Entidades/Acciones.java
+++ codigo/ProyectoFormularios/src/Entidades/Acciones.java
@@ -6,12 +6,43 @@
 
 package Entidades;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  *
  * @author sail3
  */
 public class Acciones {
+    private ArrayList<Clientes> lista = new ArrayList<Clientes>();
+    
     public void insertar(Clientes cliente) {
+        if (buscar(cliente) == null) {
+            lista.add(cliente);
+        }
+    }
+    public void modificar(Clientes cliente) {
+        Clientes cli = this.buscar(cliente);
+        if(cli != null){
+            lista.remove(cli);
+            lista.add(cliente);
+        }
         
+    }
+    public Clientes buscar(Clientes cliente){
+        Iterator it = lista.iterator();
+        while (it.hasNext()) {
+            Clientes cli = (Clientes) it.next();
+            if (cli.getCodigo().equals(cliente.getCodigo())) {
+                return cli;
+            }
+        }
+        return null;
+    }
+    public void eliminar(Clientes cliente) {
+        lista.remove(cliente);
+    }
+    public ArrayList<Clientes> listarTodos() {
+        return lista;
     }
 }
