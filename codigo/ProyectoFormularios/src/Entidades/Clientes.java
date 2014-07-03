b466b5c34178ef5eabff6188c54f81f2023b7da7
Hernan De La Cruz <hernan.delacruz.26@gmail.com>
2014-07-02 20:07:41 -0500

    Primer commit del proyecto

---
+++ codigo/ProyectoFormularios/src/Entidades/Clientes.java
@@ -0,0 +1,54 @@
+package Entidades;
+
+public class Clientes {
+    //declarar atributos
+    private String codigo;
+    //declarar atributos
+    private String nombres;
+    //declarar atributos
+    private String apellidos;
+    //declarar atributos
+    private String tipo;
+    //declarar atributos
+    private String telefono;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
